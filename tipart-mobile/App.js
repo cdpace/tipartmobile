17e9531be3170f054141f786fdc17928b7b5aaa1
Christian Pace <pace1987@live.com>
2017-09-09 16:28:54 +0200

    Started experementing and making some chagnes

--- tipart-mobile/App.js
+++ tipart-mobile/App.js
@@ -1,14 +1,13 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+
+//Components
+import TopMenu from './components/topmenu';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+        <TopMenu name="chris" />
     );
   }
 }
---
+++ tipart-mobile/components/topmenu.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+class TopMenu extends Component{
+   
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return (
+            <View>
+                
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+  
+});
+
+
+export default TopMenu;
